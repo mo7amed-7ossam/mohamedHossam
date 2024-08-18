2c578aca1bbfba97cd221a11ba9ee6355adfb0d8
Mohamed Hossam <mohamed.webdev2@gmail.com>
2024-08-18 14:32:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,82 @@
+// document.onreadystatechange = function () {
+//     if (document.readyState !== "complete") {
+//         document.querySelector("body").style.overflow = "hidden";
+//         document.querySelector(".loading").style.visibility = "visible";
+//     } else {
+//         $(".loading").fadeOut();
+//         document.querySelector("body").style.overflow = "visible";
+//     }
+// };
+
+// img
+
+for (i = 2; i <= 300; i++) {
+    $(".site-body .main-section .profile .img-box ").append("<img src='imgs/avatar/mohamedHossam (" + i + ").webp' >")
+}
+
+
+// scroll img
+
+scrollFollow = 0
+
+let fullScroll = $("html").height() - $(window).height();
+$(window).scroll(function () {
+    scrollNow = $(this).scrollTop();
+    if (scrollNow > scrollFollow && scrollNow < fullScroll) {
+        go_down = setInterval(function () {
+            if (scrollFollow < scrollNow) {
+                scrollFollow++;
+                let slide = Math.round(scrollFollow / fullScroll * 300);
+                $(".site-body .main-section .profile .img-box img").removeClass("active")
+                $(".site-body .main-section .profile .img-box  img:nth-child(" + slide + ")").addClass("active")
+                // مراقبة التمرير وتشغيل الصوت إذا وافق المستخدم
+
+
+                if (slide >= 70 && userAllowed) {
+                    sound.play().catch(function (error) {
+                        console.log('Failed to play sound:', error);
+                    });
+                } else {
+                    {
+                        sound.pause()
+                    }
+                }
+            } else {
+                clearInterval(go_down)
+            }
+        })
+
+    } else if (scrollNow < scrollFollow && scrollNow < fullScroll) {
+        go_up = setInterval(function () {
+            if (scrollFollow > scrollNow) {
+                scrollFollow--;
+                let slide = Math.round(scrollFollow / fullScroll * 300) + 1;
+                $(".site-body .main-section .profile .img-box img").removeClass("active")
+                $(".site-body .main-section .profile .img-box  img:nth-child(" + slide + ")").addClass("active")
+                // مراقبة التمرير وتشغيل الصوت إذا وافق المستخدم
+
+
+                if (slide >= 70 && userAllowed) {
+                    sound.play().catch(function (error) {
+                        console.log('Failed to play sound:', error);
+                    });
+                } else {
+                    {
+                        sound.pause()
+                    }
+                }
+            } else {
+                clearInterval(go_up)
+            }
+        })
+    }
+
+
+
+})
+
+
+
+setInterval(function(){
+    $(".hello div").toggleClass("flip")
+},3500)
