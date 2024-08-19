7fdc8ab7ae1affb88cd6542f1b3347c0a69c2b3e
Mohamed Hossam <mohamed.webdev2@gmail.com>
2024-08-19 15:15:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// document.onreadystatechange = function () {
-//     if (document.readyState !== "complete") {
-//         document.querySelector("body").style.overflow = "hidden";
-//         document.querySelector(".loading").style.visibility = "visible";
-//     } else {
-//         $(".loading").fadeOut();
-//         document.querySelector("body").style.overflow = "visible";
-//     }
-// };
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".loading").style.visibility = "visible";
+    } else {
+        $(".loading").fadeOut();
+        document.querySelector("body").style.overflow = "visible";
+    }
+};
 
 // img
 
